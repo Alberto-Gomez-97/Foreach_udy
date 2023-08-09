1ab7f2d1b1894f35785857bfbd37cd13ffafba2f
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-09 16:32:50 -0600

    Add files via upload

---
+++ Foreach_udy/Foreach_udy/Program.cs
@@ -0,0 +1,9 @@
+﻿// FOREACH PARA TRABAJAR CON COLECCIONES DE VALORES PARA ITERARLOS
+var nombre = "Alex";
+
+foreach(var letra in nombre)
+    //por cada letra en nombre 
+{
+    //separar el nombre cada letra por renglon
+    Console.WriteLine(letra);   
+}
